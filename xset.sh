96d0b7d61bc6e55647803e1655f76619f43092bd
BE404 <106765957+be404@users.noreply.github.com>
2022-08-13 11:26:53 +0530

    Add files via upload

---
+++ xset.sh
@@ -0,0 +1,25 @@
+xsetroot -name "!                "
+sleep 0.1
+xsetroot -name " @               "
+sleep 0.1
+xsetroot -name "  #              "
+sleep 0.1
+xsetroot -name "   $             "
+sleep 0.1
+xsetroot -name "    %            "
+sleep 0.1
+xsetroot -name "     ^           "
+sleep 0.1
+xsetroot -name "       &         "
+sleep 0.1 
+xsetroot -name "        *        "
+sleep 0.1
+xsetroot -name "         (       "
+sleep 0.1
+xsetroot -name "          _      "
+sleep 0.1
+xsetroot -name "           -     "
+sleep 0.1
+xsetroot -name "            +    "
+sleep 0.1
+xsetroot -name "done"
